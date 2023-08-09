b5180c38258edb71da192979fb0d0e33524ece62
Mani kandan <manicdon7@example.com>
2023-08-09 14:06:22 +0530

    static files updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: [
     "./src/**/*.{html,js}",
   ],
@@ -12,20 +11,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [], 
 }
-=======
-    content: [
-      "./src/**/*.{html,js}",
-    ],
-    theme: {
-      extend: {
-        colors :{
-          'primary':"#f59e0b",
-          'background':"#FEFDFC",
-        },
-      },
-    },
-    plugins: [],
-  }
->>>>>>> 51347473e90f42e16448f6b80f2871a9b83dbbb0
