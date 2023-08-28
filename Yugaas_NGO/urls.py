05e3e1aefb04c7774a545581d7ce3abf4a23445a
Mani kandan <manicdon7@example.com>
2023-08-28 20:42:29 +0530

    changed

--- Yugaas_NGO/urls.py
+++ Yugaas_NGO/urls.py
@@ -22,11 +22,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', views.home),
     path('',views.base),
+    path('base',views.base),
     path('aboutus',views.aboutus),
     path('services',views.our_services),
     path('card',views.card),
     path('contactus',views.contactus),
     path('faq',views.faq),
+    path('aboutus1',views.aboutus1),
+    path('aboutus2',views.aboutus2),
     #path('<int:id>/',views.detail)
 
 ]
